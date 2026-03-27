6b5ca9ba2836a2b55c56f867c307c560f5b1508e
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-03-27 18:19:27 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 260327/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -6,13 +6,17 @@ def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
 dxs = [0,1,0,-1] # 시계방향
-dys = [1,0,-1,0]
+dys = [1,0,-1,0] 
+'''
+가로로 움직이는 것 == 열번호를 조정하는 것!!!
+세로로 움직이는 것 == 행번호를 조정하는 것!!!
+'''
 
 x, y = 0, 0
 dir = 0
 arr[x][y] = 1
 
-for i in range(2, n*m+1):
+for i in range(2, n*m+1): # 총 돌아야 하는 횟수이자 채워넣야 하는 숫자로 for문 돌자
     nx, ny = x+dxs[dir], y+dys[dir]
     if not in_range(nx,ny) or arr[nx][ny] != 0:
         dir = (dir+1) % 4
