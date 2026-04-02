0498928807921992660a7f6a84ccc1b108e25aba
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-02 15:21:39 +0900

    Title: 최고의 33위치, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260402/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,18 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+answer = 0
+if n==3:
+    answer = sum(1 if grid[i][j]==1 else 0 for i in range(n) for j in range(n))
+
+else:
+    for i in range(n-2):
+        for j in range(i, i+3):
+            val = 0
+            rows = grid[i:i+3]
+            for r in rows:
+                val += sum(r[j:j+3])
+            if val > answer:
+                answer = val
+print(answer)
