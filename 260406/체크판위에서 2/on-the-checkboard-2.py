5f88060b003cf0be65fbeeeab592fbd3e84ec27b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-06 01:34:50 +0900

    Title: 체크판위에서 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260406/체크판위에서 2/on-the-checkboard-2.py
+++ 260406/체크판위에서 2/on-the-checkboard-2.py
@@ -3,12 +3,16 @@ grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 cnt = 0
+std = grid[0][0]
 for i in range(1, R-2):
     for j in range(1, C-2):
+        if grid[i][j] != std:
+            std = grid[i][j]
         for k in range(i+1, R-1):
             for l in range(j+1, C-1):
-                if grid[i][j] == 'B' and grid[k][l] == 'W':
+                if std != grid[k][l]:
                     cnt += 1
-if grid[R-1][C-1] != 'B':
+        std = grid[0][0]
+if grid[0][0] == grid[R-1][C-1]:
     cnt = 0
 print(cnt)
