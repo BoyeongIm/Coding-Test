d0f485178f0928afaae42f84cedc1778c4ac5150
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-06 01:37:55 +0900

    Title: 체크판위에서 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/체크판위에서 2/on-the-checkboard-2.py
+++ 260406/체크판위에서 2/on-the-checkboard-2.py
@@ -8,11 +8,11 @@ for i in range(1, R-2):
     for j in range(1, C-2):
         if grid[i][j] != std:
             std = grid[i][j]
-        for k in range(i+1, R-1):
-            for l in range(j+1, C-1):
-                if std != grid[k][l]:
-                    cnt += 1
-        std = grid[0][0]
+            for k in range(i+1, R-1):
+                for l in range(j+1, C-1):
+                    if std != grid[k][l]:
+                        cnt += 1
+            std = grid[0][0]
 if grid[0][0] == grid[R-1][C-1]:
     cnt = 0
 print(cnt)
