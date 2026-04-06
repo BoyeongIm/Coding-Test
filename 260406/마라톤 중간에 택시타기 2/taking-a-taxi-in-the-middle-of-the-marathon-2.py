b8c68a375382b8df85374e8d0ac86f76998571e2
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-06 11:48:19 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260406/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+def manhattan(x1, x2, y1, y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+mindist = 99999
+# Please write your code here.
+for i in range(1, n-1):
+    dist = 0
+    for j in range(n-1):
+        if j+1 == i:
+            dist += manhattan(x[j], x[j+2], y[j], y[j+2])
+        elif j==i:
+            continue
+        else:
+            dist += manhattan(x[j], x[j+1], y[j], y[j+1])
+    mindist = min(dist, mindist)
+print(mindist)
