817eb9aaa2c2c880e6b33c70ef35e2088b2bc6a3
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-08 15:24:54 +0900

    Title: G or H 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260408/G or H 3/G-or-H-3.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+x = []
+c = []
+for _ in range(n):
+    pos, char = input().split()
+    x.append(int(pos))
+    c.append(char)
+
+# Please write your code here.
+arr = [0] * max(x)
+for i in range(n):
+    arr[x[i]-1] = 1 if c[i] == "G" else 2
+window_pnts = sum(arr[:k+1])
+max_pnts = window_pnts
+for i in range(k, len(arr)):
+    window_pnts += arr[i] - arr[i-k-1]
+    max_pnts = max(max_pnts, window_pnts)
+print(max_pnts)
