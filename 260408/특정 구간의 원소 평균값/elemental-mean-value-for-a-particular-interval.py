8392127624188646cdd8c247bb88185aab13236e
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-08 15:45:35 +0900

    Title: 특정 구간의 원소 평균값, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 260408/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -9,6 +9,10 @@ for i in range(n): # 시작점
         for k in range(i, j+1):
             summ += arr[k]
         mn = summ / (j-i+1)
-        if mn in arr[i:j+1]:
-            cnt += 1
+        # if mn in arr[i:j+1]:
+        #     cnt += 1
+        for k in range(i, j+1):
+            if arr[k] == mn: 
+                cnt += 1
+                break
 print(cnt)
