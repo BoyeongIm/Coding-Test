17b95e8282160fd156c6f3436b17726763e01091
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-08 14:45:46 +0900

    Title: 구간 중 최대 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/구간 중 최대 합/max-sum-of-subarray.py
+++ 260408/구간 중 최대 합/max-sum-of-subarray.py
@@ -6,7 +6,7 @@ window_sum = sum(arr[:k])
 max_sum = window_sum
 
 for i in range(k, n):
-    window_sum = arr[i] - arr[i-k]
+    window_sum += arr[i] - arr[i-k]
     max_sum = max(max_sum, window_sum)
     
 print(max_sum)
