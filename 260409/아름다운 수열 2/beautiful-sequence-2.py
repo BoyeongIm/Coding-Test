eea2e894f0480f1f8ccb1981306ee560eb4a0f5b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-09 11:11:04 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260409/아름다운 수열 2/beautiful-sequence-2.py
+++ 260409/아름다운 수열 2/beautiful-sequence-2.py
@@ -7,7 +7,7 @@ ans = 0
 for i in range(N-M+1):
     cnt = []
     for c in range(i, i+M):
-        if A[c] in B and c not in cnt:
+        if A[c] in B:
             cnt.append(c)
     if len(cnt) == M:
         ans += 1
