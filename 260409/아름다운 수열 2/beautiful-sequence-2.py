d1e7add3189b689275567851bda1e22d39c05d36
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-09 11:53:05 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/아름다운 수열 2/beautiful-sequence-2.py
+++ 260409/아름다운 수열 2/beautiful-sequence-2.py
@@ -3,12 +3,14 @@ A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 # Please write your code here.
+from collections import Counter
 ans = 0
+cnter = Counter(B)
 for i in range(N-M+1):
     cnt = []
     for c in range(i, i+M):
         if A[c] in B:
-            cnt.append(c)
-    if len(cnt) == M:
+            cnt.append(A[c])
+    if Counter(cnt) == cnter:
         ans += 1
 print(ans)
