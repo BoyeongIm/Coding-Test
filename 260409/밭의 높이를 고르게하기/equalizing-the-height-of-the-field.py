b029390439edf2f2b6856182a6d51e0cbdeeb24d
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-09 16:06:17 +0900

    Title: 밭의 높이를 고르게하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 260409/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -2,21 +2,19 @@ N, H, T = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-import sys
-mincost = sys.maxsize
 window = arr[:T]
+cost = 0
+for w in window:
+    cost += abs(w-H)
+mincost = cost
+
+
 for i in range(T, N):
     window.pop(0)
     window.append(arr[i])
-    cnt = 0
-    for w in window:
-        if w == H:
-            cnt += 1
-    if cnt == T:
-        mincost = 0
-    else:
-        cost = 0
-        for i in range(T):
-            cost += abs(window[i]-H)
-        mincost = min(cost, mincost)
+    cost = 0
+    for i in range(T):
+        cost += abs(window[i]-H)
+    mincost = min(cost, mincost)
+
 print(mincost)
