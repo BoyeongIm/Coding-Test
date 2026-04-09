1e5739afcd012d47ebc104304d36248c77be2cfa
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-09 12:10:59 +0900

    Title: 바구니 안의 사탕 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260409/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 260409/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -12,11 +12,11 @@ arr = [0] * max(pos)
 for c, p in zip(candy, pos):
     arr[p-1] = c
 
-window = arr[:(2*K+1)]
-maxsum = sum(window)
+window = sum(arr[:(2*K+1)])
+maxsum = window
 
-for i in range(K+1, N): 
+for i in range(K+1, len(arr)-K): 
     window -= arr[i-K-1]
-    window += arr[i+K+1]
+    window += arr[i+K]
     maxsum = max(window, maxsum)
 print(maxsum)
