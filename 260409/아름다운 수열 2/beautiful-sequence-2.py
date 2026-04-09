ba124a98c507572a22d60f8eca5ede630462c43a
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-09 11:09:27 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260409/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+# Please write your code here.
+ans = 0
+for i in range(N-M+1):
+    cnt = []
+    for c in range(i, i+M):
+        if A[c] in B and c not in cnt:
+            cnt.append(c)
+    if len(cnt) == M:
+        ans += 1
+print(ans)
