ef5cd4d96c1fb363f5c2689ed2deeb9a45d8b60a
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-11 17:21:38 +0900

    Title: 삼각형 만들기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260411/삼각형 만들기/create-triangle.py
+++ 260411/삼각형 만들기/create-triangle.py
@@ -4,6 +4,11 @@ x = [p[0] for p in points]
 y = [p[1] for p in points]
 
 # Please write your code here.
+def triangle(s1, s2, x1, y1, x2, y2):
+    real_x = max(abs(s1-x1), abs(s1-x2))
+    real_y = max(abs(s2-y1), abs(s2-y2))
+    return real_x * real_y
+
 max_size = 0
 for i in range(n-2):
     x1, y1 = x[i], y[i]
@@ -12,17 +17,11 @@ for i in range(n-2):
         for k in range(j+1, n):
             x3, y3 = x[k], y[k]
             size = 0
-            if x1==x2 and y1==y3:
-                size = abs(x1-x3) * abs(y1-y2)
-            elif x1==x3 and y1==y2:
-                size = abs(x1-x2) * abs(y1-y3)
-            elif x2==x3 and y2==y1:
-                size = abs(x2-x1) * abs(y2-y3)
-            elif x2==x1 and y2==y3:
-                size = abs(x2-x3) * abs(y2-y1)
-            elif x3==x1 and y3==y2:
-                size = abs(x3-x2) * abs(y3-y1)
-            elif x3==x2 and y3==y1:
-                size = abs(x3-x1) * abs(y3-y2)
+            if (x1==x2 and y1==y3) or (x1==x3 and y1==y2):
+                size = triangle(x1, y1, x2, y2, x3, y3)
+            elif (x2==x3 and y2==y1) or (x2==x1 and y2==y3):
+                size = triangle(x2, y2, x1, y1, x3, y3)
+            elif (x3==x1 and y3==y2) or (x3==x2 and y3==y1):
+                size = triangle(x3, y3, x1, y1, x2, y2)
             max_size = max(max_size, size)
 print(max_size)
