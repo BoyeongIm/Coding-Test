f6abdfa122d3de0b464d0c38daa58c4e9a00406e
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-12 15:41:51 +0900

    Title: 겹치지 않는 선분 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 260412/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -3,6 +3,11 @@ lines = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 ans = 0
+'''
+정렬 필요 없고 그냥 모든 쌍에 대해서 봐야 하는 이유:
+앞쪽에서 A가 C와 겹친다고 셌더라도 C 자신의 차례에서는 그 정보가 없음
+한 쌍 (두개의 선분) 전체에 대해서 따져봐야 함.
+'''
 for i in range(n):
     a1, a2 = lines[i]
     crossed = False
