192c98c8311c4f29d5ac0fe877ac684e9058527c
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-12 15:37:55 +0900

    Title: 겹치지 않는 선분 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260412/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 260412/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -3,14 +3,16 @@ lines = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 lines_sorted = sorted(lines, key=lambda x:x[0])
+print(lines_sorted)
 ans = 0
 for i in range(n-1):
     a1, a2 = lines_sorted[i]
-    cnt = 1
+    crossed = False
     for j in range(i+1, n):
         b1, b2 = lines_sorted[j]
-        if a2 > b2:
-            cnt += 1
-    if cnt > 1:
-        ans += cnt
-print(n-ans)
+        if (a1-b1) * (a2-b2) < 0:
+            crossed = True
+            break
+    if not crossed:
+        ans += 1
+print(ans)
