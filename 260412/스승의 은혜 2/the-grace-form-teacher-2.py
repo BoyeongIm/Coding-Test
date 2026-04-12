2c5be0beff772171a7cc0753c0102f7165da60e5
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-12 16:51:19 +0900

    Title: 스승의 은혜 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 260412/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -7,13 +7,13 @@ P.sort()
 for i in range(N):
     student = 0
     now_price = P[i]//2
-    if B > now_price:
+    if B >= now_price:
         student += 1
         now_budget = B-now_price
     for j in range(N):
         if i==j:
             continue
-        if now_budget > P[j]:
+        if now_budget >= P[j]:
             now_budget -= P[j]
             student += 1
     max_student = max(student, max_student)
