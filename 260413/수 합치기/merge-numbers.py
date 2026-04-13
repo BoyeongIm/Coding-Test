a14bb2873c5233547a9e652bdcab37acb5890f9e
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-13 12:39:50 +0900

    Title: 수 합치기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/수 합치기/merge-numbers.py
+++ 260413/수 합치기/merge-numbers.py
@@ -2,6 +2,8 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
+# 항상 최소값 2개를 빠르게 뽑을 수 있는 구조!!!
+# 가장 작은 2개를 계속 관리할 수 있는 자료구조!! heapq!!
 import heapq
 heapq.heapify(arr)
 mincost = 0
