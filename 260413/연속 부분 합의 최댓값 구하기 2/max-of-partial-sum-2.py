b4c9549c7c7346d5159bacddc183f6761092c835
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-13 11:13:09 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260413/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+maxsum = -9999999
+for start in range(n):
+    now = 0
+    for end in range(start, n):
+        now += a[end]
+        if now < 0:
+            break
+    maxsum = max(maxsum, now)
+print(maxsum)
