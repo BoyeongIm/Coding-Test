b5af30ab26124aec2e2cdc202f3f8a72a52b7dc4
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-13 10:59:09 +0900

    Title: 동전 더하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/동전 더하기/add-coins.py
+++ 260413/동전 더하기/add-coins.py
@@ -3,6 +3,10 @@ coins = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 coins.sort(reverse=True)
+ans = 0
 for c in coins:
+    if k <= 0:
+        break
+    ans += k//c
     k %= c
-    print(k)
+print(ans)
