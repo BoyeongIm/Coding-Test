27068ec3f3213e585e828bdc28c0541278a52fbf
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-14 12:19:13 +0900

    Title: 수 채우기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260414/수 채우기/fill-in-number.py
+++ 260414/수 채우기/fill-in-number.py
@@ -4,18 +4,18 @@ n = int(input())
 cnt2 = 0
 cnt5 = 0
 if n % 2 == 1:
-    while n >= 5 and n % 2 == 1:
-        n -= 5
-        cnt5 += 1
-    cnt2 = n // 2
-    n -= 2*cnt2
+    if n % 5 == 1:
+        cnt5 = n//5 - 1
+    else: 
+        cnt5 = n//5
+
 else:
     if (n // 5) % 2 == 1:
         cnt5 = n//5 -1
     else:
         cnt5 = n//5
-    cnt2 = (n - cnt5*5) // 2
-    n -= 5*cnt5+2*cnt2
+cnt2 = (n - cnt5*5) // 2
+n -= 5*cnt5+2*cnt2
 
 if n == 0:
     print(cnt2+cnt5)
