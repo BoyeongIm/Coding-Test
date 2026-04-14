0092b8d0a8e32161c7929c0c0a2f5d2e21571042
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-14 15:46:36 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260414/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 260414/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -2,5 +2,17 @@ N = int(input())
 B = [int(input()) for _ in range(N)]
 
 # Please write your code here.
-cards = [i+1 for i in range(N)]
-print(cards)
+cards = [i+1 for i in range(2*N)]
+A = [c for c in cards if c not in B]
+A.sort()
+B.sort()
+i, j = 0, 0
+score = 0
+while i<N and j<N:
+    if A[i] > B[j]:
+        i += 1
+        j += 1
+        score += 1
+    else:
+        i += 1
+print(score)
