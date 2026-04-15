7a998e527e02268319d12d0d3be8b417bed8097b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-15 15:22:00 +0900

    Title: 폭탄 해체 작업, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/폭탄 해체 작업/the-bomb-dismantling.py
+++ 260415/폭탄 해체 작업/the-bomb-dismantling.py
@@ -13,14 +13,14 @@ totaltime, totalscore = 0, 0
 totalbomb = []
 heapq.heapify(totalbomb)
 while bombsq:
-    time, score = heapq.heappop(bombsq)
+    time, score = heapq.heappop(bombsq) # 여기서 score가 지금 음수임
     if totaltime >= time:
         if totalscore - totalbomb[0][0] + (-score) > totalscore:
+            # totalbomb에 넣을 때 양수로 만들어줬기 때문에, minscore는 양수임.
             minscore, t = heapq.heappop(totalbomb)
             totalscore -= minscore
-            totaltime -= 1
-            totaltime += 1
             totalscore += (-score)
+            # 그래서 totalbomb로 넣어줄 때는 -score로 넣어줘서, 양수로 만들어주기
             heapq.heappush(totalbomb, (-score, time))
     else:
         totaltime += 1
