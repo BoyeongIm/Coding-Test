eeddad6b075a8af68c09087c53ac3d230a038bd7
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-16 10:23:39 +0900

    Title: 자연수 M/2개의 쌍, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260416/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 260416/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -10,14 +10,15 @@ i, j = 0, N-1
 ans = -1
 
 while i < j:
+    minused = min(nums[i][1], nums[j][1])
     ssum = nums[i][0] + nums[j][0]
     if ans < ssum:
         ans = ssum
     nums[i][1] -= 1
     nums[j][1] -= 1
     if nums[i][1] == 0:
-        i += 1
+        i += minused
     if nums[j][1] == 0:
-        j -= 1
+        j -= minused
     
 print(ans)
