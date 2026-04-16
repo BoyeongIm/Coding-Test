dec386d538437e10ccaa08c4ab0d358d1197ce5b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-16 11:50:36 +0900

    Title: 최소 경로로 탈출 하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260416/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 260416/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -6,21 +6,23 @@ a = [list(map(int, input().split())) for _ in range(n)]
 def in_range(x, y):
     return 0<=x<n and 0<=y<m
 
-visited = [[False]*m for _ in range(n)]
-dxs, dys = [-1,0,0,1], [0,1,-1,0]
-q = deque([(0,0,0)])
-visited[0][0] = True
+def bfs():
+    visited = [[False]*m for _ in range(n)]
+    dxs, dys = [-1,0,0,1], [0,1,-1,0]
+    q = deque([(0,0,0)])
+    visited[0][0] = True
 
-while q:
-    x,y,dist = q.popleft()
-
-    for dx, dy in zip(dxs, dys):
-        nx,ny = x+dx, y+dy
-        if in_range(nx, ny) and not visited[nx][ny] and a[nx][ny] == 1:
-            q.append((nx, ny, dist+1))
-            visited[nx][ny] = True
+    while q:
+        x,y,dist = q.popleft()
+        if x==n-1 and y==m-1:
+            return dist
             
-if dist > 0:
-    print(dist)
-else:
-    print(-1)
+        for dx, dy in zip(dxs, dys):
+            nx,ny = x+dx, y+dy
+            if in_range(nx, ny) and not visited[nx][ny] and a[nx][ny] == 1:
+                q.append((nx, ny, dist+1))
+                visited[nx][ny] = True
+
+    return -1
+
+print(bfs())
