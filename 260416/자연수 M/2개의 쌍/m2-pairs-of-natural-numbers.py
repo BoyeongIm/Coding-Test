62b53d4df4c8b696d055d8e9d1f87cabca6d1f3b
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-16 10:05:55 +0900

    Title: 자연수 M/2개의 쌍, Time: 0ms, Memory: 0MB, Status: Memory Limit Exceed - Codetree

---
+++ 260416/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -0,0 +1,18 @@
+N = int(input())
+x, y = zip(*[tuple(map(int, input().split())) for _ in range(N)])
+x, y = list(x), list(y)
+
+# Please write your code here.
+l = []
+for i in range(N):
+    for _ in range(x[i]):
+        l.append(y[i])
+i, j = 0, N-1
+minsum = 999999
+l.sort()
+while i < j:
+    ssum = y[i] + y[j]
+    minsum = min(minsum, ssum)
+    i += 1
+    j -= 1
+print(minsum)
