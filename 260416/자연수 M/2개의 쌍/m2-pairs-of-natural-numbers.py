7fe0ca9f7eb4cf576dfbfd271a4c473861fb0775
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-16 10:21:12 +0900

    Title: 자연수 M/2개의 쌍, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260416/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 260416/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -1,17 +1,23 @@
 N = int(input())
 x, y = zip(*[tuple(map(int, input().split())) for _ in range(N)])
 
-# Please write your code here.
-l = []
+nums = []
 for i in range(N):
-    for _ in range(x[i]):
-        l.append(y[i])
+    nums.append([y[i], x[i]])
+nums.sort(lambda x:(-x[0], x[1]))
+
 i, j = 0, N-1
-minsum = 999999
-l.sort()
+ans = -1
+
 while i < j:
-    ssum = y[i] + y[j]
-    minsum = min(minsum, ssum)
-    i += 1
-    j -= 1
-print(minsum)
+    ssum = nums[i][0] + nums[j][0]
+    if ans < ssum:
+        ans = ssum
+    nums[i][1] -= 1
+    nums[j][1] -= 1
+    if nums[i][1] == 0:
+        i += 1
+    if nums[j][1] == 0:
+        j -= 1
+    
+print(ans)
