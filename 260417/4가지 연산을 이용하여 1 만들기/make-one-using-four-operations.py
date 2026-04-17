810f49e66f3bf51727a82db7974e1dfd757954cb
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-17 12:55:22 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 0ms, Memory: 0MB, Status: Memory Limit Exceed - Codetree

---
+++ 260417/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,26 @@
+N = int(input())
+from collections import deque
+# Please write your code here.
+def cal(i, num):
+    if i == 0:
+        return num-1
+    if i == 1:
+        return num+1
+    if i == 2 and num % 2 == 0:
+        return num//2
+    if i == 3 and num % 3 == 0:
+        return num//3
+    return num
+
+q = deque([(N, 0)])
+while q:
+    num, times = q.popleft()
+    if num == 1:
+        print(times)
+        break
+    for i in range(4):
+        result = cal(i, num)
+        if num == result:
+            q.append((result, times))
+        else:
+            q.append((result, times+1))
