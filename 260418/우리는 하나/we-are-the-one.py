41f77dd51f406f8d3548781ce95ef2623ef735e9
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-18 18:01:22 +0900

    Title: 우리는 하나, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260418/우리는 하나/we-are-the-one.py
@@ -0,0 +1,37 @@
+from collections import deque
+from itertools import combinations_with_replacement, combinations
+n, k, u, d = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+dots = [i for i in range(n)]
+dots = list(combinations_with_replacement(dots,2))
+starts = list(combinations(dots, k))
+dxs = [1,0,0,-1]
+dys = [0,1,-1,0]
+maxcities = -1
+
+for comb in starts:
+    cities = k
+    visited = [[False]*n for _ in range(n)]
+    q = deque([])
+
+    for x, y in comb:
+        q.append((x,y))
+        visited[x][y]=True
+    while q:
+        x, y = q.popleft()
+        height1 = grid[x][y]
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if in_range(nx, ny) and not visited[nx][ny]:
+                height2 = grid[nx][ny]
+                if u <= abs(height1-height2) <= d:
+                    cities += 1
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+    maxcities = max(cities, maxcities)
+print(maxcities)
