3df54498b70b53c85a944c07e16fdb61860a5aef
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-19 12:23:10 +0900

    Title: 그래프 탐색, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/그래프 탐색/graph-traversal.py
+++ 260419/그래프 탐색/graph-traversal.py
@@ -12,11 +12,19 @@ ans = 0
 
 def dfs(node):
     global ans
+    '''
+    DFS는 "연결된 애들만 따라가는 구조"이다
+    따라서:
+    전체 노드를 도는 게 아니라 ❌
+    현재 노드의 이웃만 본다 ⭕
+    '''
     for nxt in graph[node]:
         if not visited[nxt]:
             ans += 1
             visited[nxt] = True
             dfs(nxt)
+            
+# 시작 노드는 방문 처리 해주기!!!!
 visited[1]=True
 dfs(1)
 print(ans)
