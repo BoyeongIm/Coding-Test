f6416fce730b424cd51beb7efada768e2b653525
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-19 12:44:17 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 260419/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -2,13 +2,13 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-visited = [[False]*m for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
 def can_go(x, y):
     return 0<=x<n and 0<=y<m and grid[x][y] == 1
 
 def dfs(x, y):
-    if x==n-1 and y==m-1:
-        return True
+    # if x==n-1 and y==m-1:
+    #     return True
 
     dxs = [1,0]
     dys = [0,1]
@@ -16,13 +16,20 @@ def dfs(x, y):
     for dx, dy in zip(dxs, dys):
         nx, ny = x+dx, y+dy
         if can_go(nx, ny) and not visited[nx][ny]:
-            visited[nx][ny] = True
-            if dfs(nx, ny):
-                return True
-    return False
+            visited[nx][ny] = 1
+            dfs(nx, ny)
+    #         if dfs(nx, ny):
+    #             return True
+    # return False
 
-visited[0][0] = True
-if dfs(0,0):
-    print(1)
-else:
-    print(0)
+visited[0][0] = 1
+dfs(0,0)
+print(visited[n-1][m-1])
+# if dfs(0,0):
+#     print(1)
+# else:
+#     print(0)
+
+'''
+그냥 visited로 도착부분만 확인할 수도 있음!!!!!
+'''
