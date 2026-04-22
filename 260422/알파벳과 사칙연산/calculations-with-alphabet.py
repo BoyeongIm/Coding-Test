8287b33329f82939dfcce524a1b3540d3f92a14d
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-22 12:46:28 +0900

    Title: 알파벳과 사칙연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260422/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 260422/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -18,22 +18,24 @@ def dfs(idx, result):
     else:
         op = '+'
     if curr in engdict:    
+        val = engdict[curr]
         if op == '+':
-            newresult = result + engdict[curr]
+            newresult = result + val
         elif op == '-':
-            newresult = result - engdict[curr]
+            newresult = result - val
         elif op == '*':
-            newresult = result * engdict[curr]
+            newresult = result * val
         dfs(idx+2, newresult)
     else:
         for i in range(1, 5):  
             engdict[curr] = i
+            val = engdict[curr]
             if op == '+':
-                newresult = result + engdict[curr]
+                newresult = result + val
             elif op == '-':
-                newresult = result - engdict[curr]
+                newresult = result - val
             elif op == '*':
-                newresult = result * engdict[curr]
+                newresult = result * val
             dfs(idx+2, newresult)
             del engdict[curr]   
     return
