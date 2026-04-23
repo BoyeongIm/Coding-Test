70a8366278e7b9d09435805a7618c1fa29bfe6f1
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-23 22:22:05 +0900

    Title: 1차원 윷놀이, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/1차원 윷놀이/yutnori-1d.py
+++ 260423/1차원 윷놀이/yutnori-1d.py
@@ -4,15 +4,18 @@ nums = list(map(int, input().split()))
 player = [1 for i in range(k)]
 maxscore = -1
 def play(idx):
+    # 매번 점수 계산하면서 최대점수 갱신
+    global maxscore
+    score = 0
+    for p in player:
+        score += (p >= m)
+    maxscore = max(maxscore, score)
     if idx == n:
-        global maxscore
-        score = 0
-        for p in player:
-            if p >= m:
-                score += 1
-        maxscore = max(maxscore, score)
         return
     for i in range(k):
+        # 이미 m을 넘었다면 움직일 필요 없음.
+        if player[i] >= m:
+            continue
         player[i] += nums[idx]
         play(idx+1)
         player[i] -= nums [idx]
