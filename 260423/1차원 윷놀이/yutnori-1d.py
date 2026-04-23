2678d01e1ef21a7bc76d6e15a66cd9fd441918a8
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-23 18:12:54 +0900

    Title: 1차원 윷놀이, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/1차원 윷놀이/yutnori-1d.py
+++ 260423/1차원 윷놀이/yutnori-1d.py
@@ -1,23 +1,18 @@
 n, m, k = map(int, input().split())
 nums = list(map(int, input().split()))
 
-# Please write your code here.
-from collections import defaultdict
-player = defaultdict(int)
-for i in range(1, k+1):
-    player[i] = 1
-
+player = [1 for i in range(k)]
 maxscore = -1
 def play(idx):
     if idx == n:
         global maxscore
         score = 0
         for p in player:
-            if player[p] >= m:
+            if p >= m:
                 score += 1
         maxscore = max(maxscore, score)
         return
-    for i in range(1, k+1):
+    for i in range(k):
         player[i] += nums[idx]
         play(idx+1)
         player[i] -= nums [idx]
