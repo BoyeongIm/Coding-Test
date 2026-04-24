7112811bc8f32cbaf5323ab71a9fba5854a63780
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-24 12:45:50 +0900

    Title: 정수 사각형 최대 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 260424/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -9,7 +9,7 @@ def initialize():
     dp[0][0] = grid[0][0]
     for i in range(1, n):
         dp[i][0] = dp[i-1][0]+grid[i][0]
-    for j in range(n):
+    for j in range(1, n):
         dp[0][j] = dp[0][j-1]+grid[0][j]
 
 initialize()
