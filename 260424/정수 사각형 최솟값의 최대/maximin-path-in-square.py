4c44db14d680b47b108e35a567757a9cdf697284
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-24 13:28:43 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 260424/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -16,9 +16,9 @@ ans = -sys.maxsize
 
 if n == 1:
     ans = grid[0][0]
-    
+
 for i in range(1, n):
     for j in range(1, n):
-        dp[i][j] = min(dp[i-1][j], dp[i][j-1], grid[i][j])
-        ans = max(ans, dp[i][j])
-print(ans)
+        dp[i][j] = max(min(dp[i-1][j], grid[i][j]), min(dp[i][j-1], grid[i][j]))
+        
+print(dp[n-1][n-1])
