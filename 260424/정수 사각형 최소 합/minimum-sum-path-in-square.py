bc8a0c9e311b89e55549e23b744e4d0362593c03
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-24 13:11:01 +0900

    Title: 정수 사각형 최소 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ 260424/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -7,14 +7,14 @@ def initialize():
     dp[0][n-1] = grid[0][n-1]
     for i in range(1, n):
         dp[i][n-1] = dp[i-1][n-1]+grid[i][n-1]
-    for j in range(0, n-1, -1):
+    for j in range(n-2, -1, -1):
         dp[0][j] = dp[0][j+1]+grid[0][j]
 
 initialize()
 
 for i in range(1, n):
-    for j in range(0, n-1, -1):
-        dp[i][j] = max(dp[i-1][j]+grid[i][j],
+    for j in range(n-2, -1, -1):
+        dp[i][j] = min(dp[i-1][j]+grid[i][j],
                         dp[i][j+1]+grid[i][j])
 
 ans = dp[n-1][0]
