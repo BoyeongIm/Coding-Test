c3e0309b7a923697b25ea4240e5c9906a82c5244
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-24 14:10:09 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 260424/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -10,12 +10,16 @@ dp = [[0]*n for _ in range(n)]
 def dfs(x, y):
     if dp[x][y] != 0:
         return dp[x][y]
+    # 초기화
     dp[x][y] = 1
     for dx, dy in zip(dxs, dys):
         nx, ny = x+dx, y+dy
         if in_range(nx, ny) and grid[nx][ny] > grid[x][y]:
+            # 조건을 만족하면, 현재 x,y의 dp값을 갱신해줌!!!
+            ## 지금 현재 dp에 저장되어 있는 값이랑, 새롭게 탐색하는 nx,ny로 갔을 때의 결과와 비교
             dp[x][y] = max(dp[x][y], 1+dfs(nx, ny))
     return dp[x][y]
+
 ans = -1
 for i in range(n):
     for j in range(n):
