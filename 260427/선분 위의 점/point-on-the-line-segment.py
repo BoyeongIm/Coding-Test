b4f4e020f3db0b608717e33c53392b3764fc5040
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-04-27 20:02:51 +0900

    Title: 선분 위의 점, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/선분 위의 점/point-on-the-line-segment.py
+++ 260427/선분 위의 점/point-on-the-line-segment.py
@@ -3,11 +3,28 @@ points = list(map(int, input().split()))
 segments = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
-for x, y in segments:
-    ans = 0
-    for p in points:
-        if p < x or p > y:
-            continue
+points.sort()
+def lower_bound(q):
+    l, r = 0, n-1
+    while l<=r:
+        mid = (l+r)//2
+        if q <= points[mid]:
+            r = mid-1
         else:
-            ans += 1
-    print(ans)
+            l = mid+1
+    return l
+
+def upper_bound(q):
+    l, r = 0, n-1
+    while l<=r:
+        mid = (l+r)//2
+        if q < points[mid]:
+            r = mid-1
+        else:
+            l = mid+1
+    return r
+
+for x, y in segments:
+    left = lower_bound(x)
+    right = upper_bound(y)
+    print(right-left+1)
