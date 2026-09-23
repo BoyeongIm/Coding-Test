f173de779ddd7eae5f7533378c7e46c65499db4d
BoYeong IM <121472614+BoyeongIm@users.noreply.github.com>
2026-09-24 01:32:02 +0900

    [Passed] 아기 고래의 첫 항해 (272ms, 25MB)

--- samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.py
+++ samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.py
@@ -5,6 +5,12 @@ ocean = [list(map(int, input().split())) for _ in range(N)]
 1. 인접 탐험
     - 이동 우선순위: 현재 방향 > 90도 반시계 > 90도 시계 > 180도 회전
 
+2. (인접한 칸에 방문 가능한 바다가 없다면) 가장 가까운 바다로 이동
+    - 암초 못지나감
+    - 이미 방문한 바다 지나갈 수 있음
+    - 가장 가까운 칸이 여러 개라면, 행 번호 > 열 번호 우선순위로 작은 칸 선택
+    - 선택한 칸까지 최단 거리로 이동, 가능한 경로가 여러 개라면 좌>하>우>상 순서로 우선순위 
+    - 마지막 이동 방향으로 바라보는 방향 갱신
 '''
 dirs = {1:(-1,0), 2:(1,0), 3:(0,-1), 4:(0,1)} # 상 하 좌 우
 straight = {1:1, 2:2, 3:3, 4:4}
@@ -13,29 +19,23 @@ clock = {1:4, 4:2, 2:3, 3:1}
 opposite = {1:2, 2:1, 3:4, 4:3}
 priorities = [straight, counterclock, clock, opposite]
 
-'''
-2. (인접한 칸에 방문 가능한 바다가 없다면) 가장 가까운 바다로 이동
-- 암초 못지나감
-- 이미 방문한 바다 지나갈 수 있음
-- 가장 가까운 칸이 여러 개라면, 행 번호 > 열 번호 우선순위로 작은 칸 선택
-- 선택한 칸까지 최단 거리로 이동, 여러 개라면 좌>하>우>상 순서로 우선순위 
-- 마지막 이동 방향으로 바라보는 방향 갱신
-'''
 from collections import deque
 
+# BFS & 주어진 지점부터 모든 지점까지의 거리를 저장한 2차원 배열 리턴
 def get_dist(tx, ty):
     dist = [[-1]*N for _ in range(N)]
+    directions = [[0]*N for _ in range(N)]
     dist[tx][ty] = 0
     q = deque([(tx, ty)])
     while q:
         cx, cy = q.popleft()
-        for dx, dy in zip(dxs, dys):
+        for i, (dx, dy) in enumerate(zip(dxs, dys)):
             nx, ny = cx+dx, cy+dy
             if 0<=nx<N and 0<=ny<N and dist[nx][ny]==-1 and ocean[nx][ny]==0:
                 dist[nx][ny] = dist[cx][cy] + 1
+                directions[nx][ny] = i
                 q.append((nx, ny))
-    return dist
-
+    return dist, directions
 
 dxs, dys = [0,1,0,-1],[-1,0,1,0] # 좌 하 우 상
 visited = set()
@@ -48,18 +48,24 @@ for i in range(N):
         if ocean[i][j] == 0:
             total_ocean += 1
 
+# 헤엄칠 수 있는 모든 바다를 방문하면 종료
 while len(visited) < total_ocean:
+    # 1단계: 우선순위대로 방문하지 않은 바다 칸으로 이동
     for p in priorities:
-        dx, dy = dirs[p[cd]]
-        nx, ny = cx+dx, cy+dy
+        ## 지금 우선순위 대로 회전해서 방향 바꾸기
         nd = p[cd]
+        dx, dy = dirs[nd]
+        nx, ny = cx+dx, cy+dy
         if 0<=nx<N and 0<=ny<N and ocean[nx][ny] == 0 and (nx, ny) not in visited:
             visited.add((nx, ny))
             cx,cy,cd = nx,ny,nd
             print(nx+1, ny+1)   
             break
+    # 1단계에서 처리될 수가 없어서 (인접한 칸에 방문 가능한 바다가 없어서) 앞의 for문이 break로 끝나지 않았을 때 else로 처리하기
     else:
-        dist = get_dist(cx, cy)
+        ## 지금 현재 위치를 기준으로 가장 가까운 칸을 찾기 위해서, 현재 위치 기준 모든 위치까지의 거리 계산
+        ## 이동할 타깃 위치를 찾는다 !!!
+        dist, directions = get_dist(cx, cy)
         target = (99,-1,-1)
         for i in range(N):
             for j in range(N):
@@ -72,14 +78,9 @@ while len(visited) < total_ocean:
                         elif target[1] == i and target[2] > j:
                             target = (dist[i][j], i, j)
 
-        td, tx, ty = target
-        tdist = get_dist(tx, ty)
-        while (cx, cy) != (tx, ty):
-            for i in range(4):
-                nx, ny = cx+dxs[i], cy+dys[i]
-                if 0<=nx<N and 0<=ny<N and tdist[nx][ny] == tdist[cx][cy]-1:
-                    cx, cy = nx, ny
-                    cd = [3,2,4,1][i]
-                    break
+        tdistance, tx, ty = target
+        cx, cy = tx, ty
+        cd = [3,2,4,1][directions[tx][ty]]
+
         visited.add((cx, cy))    
         print(cx+1, cy+1)
